5ab7c350edbbd3849dd1e46664063ac0682c40d9
pasvistelik <pasvistelik@gmail.com>
2017-11-17 11:16:10 +0300

    Update

--- src/positionsLocalArchive.js
+++ src/positionsLocalArchive.js
@@ -1,9 +1,17 @@
 var archive = [];
+var posIndex = 0;
 
 class Position {
     constructor(vehicle){
-        //this.previousPosition = vehicle.lastPositionObj;
+
+        this.previousPosition = vehicle.lastPositionObj;
+        this.previousPositionId = (vehicle.lastPositionId == null) ? null : vehicle.lastPositionId;
+        
+        this.positionId = posIndex++;
+
         this.nextPosition = null;
+        this.nextPositionId = null;
+        
         this.lat = vehicle.lat;
         this.lng = vehicle.lng;
         //this.timestamp = vehicle.timestamp;
@@ -14,10 +22,17 @@ class Position {
         this.dayOfWeek = tmpDate.getDay();// 0 -- 6
         this.timeSeconds = tmpDate.getSeconds() + 60 * tmpDate.getMinutes() + 3600 * tmpDate.getHours();//time in seconds from day begin
 
-        this.routeId = vehicle.routeId;
+        this.routeId = (vehicle.route != null) ? vehicle.route.routeId : null;
+        this.wayId = (vehicle.way != null) ? vehicle.way.wayId : null;
+        this.tripId = (vehicle.trip != null) ? vehicle.trip.tripId : null;
 
-        //vehicle.lastPositionObj.nextPosition = this;
-        //vehicle.lastPositionObj = this;
+        if(vehicle.lastPositionObj != null){
+            vehicle.lastPositionObj.nextPosition = this;
+            vehicle.lastPositionObj.nextPositionId = this.positionId;
+            vehicle.lastPositionObj.previousPosition = null;
+        }
+        vehicle.lastPositionObj = this;
+        vehicle.lastPositionId = this.positionId;
     }
 }
 class PositionsLocalArchive {
